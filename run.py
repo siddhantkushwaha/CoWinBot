e745256b610cdc70c78d62594fa84c21498f0d28
siddhantkushwaha <k16.siddhant@gmail.com>
2021-05-07 07:47:47 +0530

    update requests in each run

--- run.py
+++ run.py
@@ -4,8 +4,9 @@ from fetcher import parse_pincode_age_requests, fetch
 from notifier import send_notifications
 
 if __name__ == '__main__':
-    all_user_req = parse_pincode_age_requests()
     while True:
+        all_user_req = parse_pincode_age_requests()
+
         fetch(all_user_req)
         send_notifications(all_user_req)
 
