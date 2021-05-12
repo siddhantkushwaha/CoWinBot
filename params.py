1922e039f515b1bf1c40ea5c1ff995833a4dcacc
siddhantkushwaha <k16.siddhant@gmail.com>
2021-05-12 17:19:52 +0530

    remove dirs from params

--- params.py
+++ params.py
@@ -9,13 +9,4 @@ with open(os.path.join(root_dir, "configs", "config.json"), "r") as fp:
 with open(os.path.join(root_dir, "configs", "token.json"), "r") as fp:
     tokens = json.load(fp)
 
-requests_dir = os.path.join(root_dir, 'data', 'requests')
-os.makedirs(requests_dir, exist_ok=True)
-
-data_dir = os.path.join(root_dir, 'data', 'cowin')
-os.makedirs(data_dir, exist_ok=True)
-
-notification_state_dir = os.path.join(root_dir, 'data', 'notificationState')
-os.makedirs(notification_state_dir, exist_ok=True)
-
 raw_data_dir = os.path.join(root_dir, 'rawData')
