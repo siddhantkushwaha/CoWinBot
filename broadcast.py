0923360b404c962d480c01dd7f7c46cddf4e3c87
siddhantkushwaha <k16.siddhant@gmail.com>
2021-05-19 18:43:00 +0530

    comment broadcast code

--- broadcast.py
+++ broadcast.py
@@ -2,6 +2,7 @@
 #
 # import telebot
 # from db import dbHelper
+# from util import get_key
 #
 # second_text = "Data for your pincode may not be getting checked as frequently as you'd like." \
 #               "\n\nYou can help update bot's database more frequently by running a process available on Google Drive here https://cutt.ly/AbJbynB" \
@@ -12,6 +13,12 @@
 #     user_id = user_info['userId']
 #
 #     res = telebot.send_photo_url(user_id, second_text, screenshot_url)
-#     print(user_id, res)
+#     inf = {
+#         'first_name': get_key(res, ['result', 'chat', 'first_name']),
+#         'last_name': get_key(res, ['result', 'chat', 'last_name']),
+#         'username': get_key(res, ['result', 'chat', 'username'])
+#     }
+#     ret = dbHelper.update_user_info_set(user_id, inf)
 #
+#     print(inf, ret)
 #     time.sleep(2)
